d78231ddc8026b80c1bf17acec7a7ff20f758bbc
Mohamed Raafat <mohamedra2fat@gmail.com>
2014-04-28 20:00:22 +0300

    Update models.py
    Added some comments for the team members to understand

--- AlternativeLogging/AlternateLogging/models.py
+++ AlternativeLogging/AlternateLogging/models.py
@@ -6,6 +6,7 @@ class User(Model):
     email = CharField(max_length=128,primary_key=True)
     password = CharField(max_length=64)
  
+ #Function that return a category of password that corresponds to the user password
     def categories(self):
         return [cat for cat in [Category.objects.get(id=cid) for cid in self.password.split()]]
     
@@ -18,7 +19,7 @@ class Category(Model):
  
     def __unicode__(self):
         return self.name
-        
+        #Function to return a specific picture
     def get_pictures(self):
         return [pic for pic in Picture.objects.filter(category=self)]
         
