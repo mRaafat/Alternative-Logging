7aeca742667fcbf829d7d59afcfb08684c7d7077
Mohamed Raafat <mohamedra2fat@gmail.com>
2014-04-30 14:12:18 +0200

    Modifying models.py

--- AlternativeLogging/AlternateLogging/models.py
+++ AlternativeLogging/AlternateLogging/models.py
@@ -3,15 +3,20 @@ from django.db.models import *
 from django.forms import ModelForm, PasswordInput
 
 # Create your models here.
-class User(Model):
-	email = CharField(max_length=100,primary_key=True)
-	password = CharField(max_length=50)
-	def categories(self):
-		categoryPasswords = {}
-
-
-	def __unicode__(self):
-		return self.email
+from django.db.models import *
+ 
+# Create your models here.
+ 
+class User(Model): 
+    email = CharField(max_length=128,primary_key=True)
+    password = CharField(max_length=64)
+ 
+    def categories(self):
+        return [cat for cat in [Category.objects.get(id=cid) for cid in self.password.split()]]
+    
+    def __unicode__(self):
+        return self.email
+ 
 
 class UserForm(ModelForm):
 	class Meta:
@@ -20,24 +25,6 @@ class UserForm(ModelForm):
 			'password' : PasswordInput(),
 		}
 
-	
-=======
- 
-# Create your models here.
- 
-class User(Model): 
-    email = CharField(max_length=128,primary_key=True)
-    password = CharField(max_length=64)
- 
- #Function that return a category of password that corresponds to the user password
-    def categories(self):
-        return [cat for cat in [Category.objects.get(id=cid) for cid in self.password.split()]]
-    
-    def __unicode__(self):
-        return self.email
- 
- 
->>>>>>> d78231ddc8026b80c1bf17acec7a7ff20f758bbc
 class Category(Model):
     name = CharField(max_length = 64)
  
@@ -54,3 +41,4 @@ class Picture(Model):
     
     def __unicode__(self):
         return self.url
+
